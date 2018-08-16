c9ba70c5d96cd6750f821129d121ea7592e19c88
AubreyNeimeier <neimeier.1@osu.edu>
2018-08-16 08:56:38 -0700

    incorporates the abstract solutions and notes

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function getFirstSelector(selector){
 function nestedTarget(nested, target){
     return document.querySelector('#nested .target');
   }
+// MY SOLUTION WHICH WORKS PERFECTLY FINE
+// the i + n doesn't make sense to me. it seems like its adding the 
+//incrementer to n and overriding previous value instead of simply adding n 
+//to whatever exists in the inner HTML. see line 32!!!! 
 
   function increaseRankBy(n){
       const x = document.getElementsByClassName('ranked-list');
@@ -14,6 +18,45 @@ function nestedTarget(nested, target){
       };
   }
 
+// FLATIRONS SOLUTION - more complex and abstract
+/*
+function increaseRankBy(n) {
+    const rankedLists = document.querySelectorAll('.ranked-list')
+  
+    // this first for loops grabs the first two groups of ranked-lists
+    for (let i = 0, l = rankedLists.length; i < l; i++) {
+      let children = rankedLists[i].children
+    
+    // Now we are iterating through the two groups and increasing each by n. 
+      for (let j = 0, k = children.length; j < k; j++) {
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n
+      }
+    }
+  }
+ */
+
+// MY SOLUTION. Works but is not abstract at all. 
+
   function deepestChild(){
       return document.querySelector('#grand-node div div div div');
   }
+
+// FLATIRONS SOLUTION will find the deepest child without know exactly how deep it is. 
+ /*
+
+ // LOGIC is WHILE next deepest element exists--- keep reassigning the node. 
+ // The FINAL remaining NODE(aka the last definition of node) will be the deepest element
+
+  function deepestChild() {
+    let node = document.getElementById('grand-node')
+    let nextNode = node.children[0]
+  
+    while (nextNode) {
+      node = nextNode
+      nextNode = node.children[0]
+    }
+  
+    return node
+  }
+
+  */ 

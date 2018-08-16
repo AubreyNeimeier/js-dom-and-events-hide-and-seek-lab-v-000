812029a63d139647de4b50d265e96b919ea7b52b
AubreyNeimeier <neimeier.1@osu.edu>
2018-08-15 19:39:26 -0700

    passes half tests

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector){
+    return document.querySelector(selector)
+}
+
+function nestedTarget(nested, target){
+    return document.querySelector('#nested .target')
+  }
+
+  function increaseRankBy(n){
+      const x = document.getElementsByClassName('ranked-list');
+
+      for (let i = 0; i < x.length; i ++){
+          x[i].innerHTML = (i + n).toString()
+      }
+  }
